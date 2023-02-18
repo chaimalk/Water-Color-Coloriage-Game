1f202d12d4c376461e55e35b3e6c8a14873dc1c4
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-19 00:22:01 +0100

    Create watercolor.ads

---
+++ watercolor.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/1711074865",
+    "admobInterstitial": "ca-app-pub-1010770342437944/2832584845",
+    "applovinBanner": "b3ff1b070bfc1281",
+    "applovinInterstitial": "4af7269cff7f13bb",
+    "unityGameId": "5170985",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
